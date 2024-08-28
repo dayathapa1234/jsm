368e8dde55a1a5b4530770f88910906e5a3304c7
dayathapa1234 <dayathapa1234@gmail.com>
2024-08-28 22:04:18 +0100

    working on homepage component such as header and total balance box

---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { Doughnut } from 'react-chartjs-2';
+
+const DoughnutChart = ({accounts}: DoughnutChartProps) => {
+    const data = {}
+  return <Doughnut data={[]}/>
+}
+
+export default DoughnutChart
